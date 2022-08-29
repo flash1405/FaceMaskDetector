a90d6cbacaca51221d820a0247f5398f4b441060
Aryan Gupta <47558578+flash1405@users.noreply.github.com>
2022-08-28 19:49:51 -0500

    Add files via upload

---
+++ detect_mask_image.py
@@ -0,0 +1,141 @@
+# USAGE
+# python detect_mask_image.py --image examples/example_01.png
+
+# import the necessary packages
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
+import numpy as np
+import argparse
+import cv2
+import pickle
+import os
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+	help="path to input image")
+ap.add_argument("-f", "--face", type=str,
+	default="face_detector",
+	help="path to face detector model directory")
+ap.add_argument("-m", "--model", type=str,
+	default="mask_detector.model",
+	help="path to trained face mask detector model")
+ap.add_argument("-c", "--confidence", type=float, default=0.5,
+	help="minimum probability to filter weak detections")
+ap.add_argument("-d", "--detector", required=True,
+	help="path to OpenCV's deep learning face detector")
+ap.add_argument("-m2", "--embedding-model", required=True,
+	help="path to OpenCV's deep learning face embedding model")
+ap.add_argument("-r", "--recognizer", required=True,
+	help="path to model trained to recognize faces")
+ap.add_argument("-l", "--le", required=True,
+	help="path to label encoder")
+args = vars(ap.parse_args())
+
+# # load our serialized face detector from disk
+# print("[INFO] loading face detector...")
+# protoPath = os.path.sep.join([args["detector"], "deploy.prototxt"])
+# modelPath = os.path.sep.join([args["detector"],
+# 	"res10_300x300_ssd_iter_140000.caffemodel"])
+# detector = cv2.dnn.readNetFromCaffe(protoPath, modelPath)
+
+# load our serialized face embedding model from disk
+print("[INFO] loading face recognizer...")
+embedder = cv2.dnn.readNetFromTorch(args["embedding_model"])
+
+# load the actual face recognition model along with the label encoder
+recognizer = pickle.loads(open(args["recognizer"], "rb").read())
+le = pickle.loads(open(args["le"], "rb").read())
+
+# load our serialized face detector model from disk
+print("[INFO] loading face detector model...")
+prototxtPath = os.path.sep.join([args["face"], "deploy.prototxt"])
+weightsPath = os.path.sep.join([args["face"],
+	"res10_300x300_ssd_iter_140000.caffemodel"])
+net = cv2.dnn.readNet(prototxtPath, weightsPath)
+
+# load the face mask detector model from disk
+print("[INFO] loading face mask detector model...")
+model = load_model(args["model"])
+
+# load the input image from disk, clone it, and grab the image spatial
+# dimensions
+image = cv2.imread(args["image"])
+orig = image.copy()
+(h, w) = image.shape[:2]
+
+# construct a blob from the image
+blob = cv2.dnn.blobFromImage(image, 1.0, (300, 300),
+	(104.0, 177.0, 123.0))
+# pass the blob through the network and obtain the face detections
+print("[INFO] computing face detections...")
+net.setInput(blob)
+detections = net.forward()
+
+# loop over the detections
+for i in range(0, detections.shape[2]):
+	# extract the confidence (i.e., probability) associated with
+	# the detection
+	confidence = detections[0, 0, i, 2]
+
+	# filter out weak detections by ensuring the confidence is
+	# greater than the minimum confidence
+	if confidence > args["confidence"]:
+		# compute the (x, y)-coordinates of the bounding box for
+		# the object
+		box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+		(startX, startY, endX, endY) = box.astype("int")
+
+		# ensure the bounding boxes fall within the dimensions of
+		# the frame
+		(startX, startY) = (max(0, startX), max(0, startY))
+		(endX, endY) = (min(w - 1, endX), min(h - 1, endY))
+
+		# extract the face ROI, convert it from BGR to RGB channel
+		# ordering, resize it to 224x224, and preprocess it
+		face = image[startY:endY, startX:endX]
+		faceBlob = cv2.dnn.blobFromImage(face, 1.0 / 255, (96, 96),
+			(0, 0, 0), swapRB=True, crop=False)
+		face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+		face = cv2.resize(face, (224, 224))
+		face = img_to_array(face)
+		face = preprocess_input(face)
+		face = np.expand_dims(face, axis=0)
+
+		# pass the face through the model to determine if the face
+		# has a mask or not
+		(mask, withoutMask) = model.predict(face)[0]
+		# determine the class label and color we'll use to draw
+		# the bounding box and text
+		label = "Mask" if mask > withoutMask else "No Mask"
+		color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
+		
+		if label == "No Mask":
+			embedder.setInput(faceBlob)
+			vec = embedder.forward()
+			# perform classification to recognize the face
+			preds = recognizer.predict_proba(vec)[0]
+			j = np.argmax(preds)
+			proba = preds[j]
+			name = le.classes_[j]
+			# draw the bounding box of the face along with the associated
+			# probability
+			text = "{}: {:.2f}%".format(name, proba * 100)
+			y = startY - 10 if startY - 10 > 10 else startY + 10
+			cv2.putText(image, text, (startX, y),
+				cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+		else:	
+			# include the probability in the label
+			label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
+
+			# display the label and bounding box rectangle on the output
+			# frame
+			cv2.putText(image, label, (startX, startY - 10),
+				cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+			
+		cv2.rectangle(image, (startX, startY), (endX, endY), color, 2)
+
+# show the output image
+cv2.imshow("Output", image)
+cv2.waitKey(0)
